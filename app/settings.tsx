a890c53c53fa9be3f73d847d984cebb99a4019bf
Andrey Lechev <andrey@lechev.me>
2025-01-16 09:11:23 +0100

    Add some debug info to the settings screen

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,14 +1,42 @@
+import { profiles$ as _profiles$, user$ as _user$ } from '@/state/state';
+import { supabase } from '@/state/state';
+
+// components
+import { Text, View } from 'react-native';
 import Page from '@/components/Page';
+import SmallButton from '@/components/SmallButton';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { globalStyles } from '@/constants/GlobalStyles';
 
-export default function SettingsScreen() {
+// styles
+import { globalStyles } from '@/constants/GlobalStyles';
+import { observer } from '@legendapp/state/react';
+
+function SettingsScreen() {
+  const profiles = _profiles$.get();
+  function displayProfiles() {
+    console.log('profiles', profiles);
+  }
+
+  function logout() {
+    supabase.auth.signOut();
+  }
+
   return (
     <Page hasHeader={true}>
       <ThemedView style={globalStyles.titleContainer}>
         <ThemedText type="title">Settings </ThemedText>
+        <SmallButton title="Log Out" onPress={logout} />
       </ThemedView>
+      <View style={{ padding: 16 }}>
+        <Text>Current user id: {_user$.get()?.id}</Text>
+      </View>
+      <View style={{ padding: 16 }}>
+        <SmallButton title="Dump profiles" onPress={displayProfiles} />
+        <Text>Profiles no: {Object.keys(_profiles$.get() || {}).length}</Text>
+      </View>
     </Page>
   );
 }
+
+export default observer(SettingsScreen);
